12d9a691080c8083d0e913031e9aab475e062cb4
Sabaria21 <114469120+Sabaria21@users.noreply.github.com>
2024-03-06 00:29:01 +0800

    Add files via upload

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,3 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import streamlit as st
-from babel.numbers import format_currency
+import streamlit as st 
 
-sns.set(style='dark')
+st.write("Hello World")
