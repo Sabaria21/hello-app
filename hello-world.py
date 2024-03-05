c7e550e00490a163ae84792a357d766a41e90104
Sabaria21 <114469120+Sabaria21@users.noreply.github.com>
2024-03-06 00:44:17 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 
 # Judul dashboard
 st.title('Dashboard')
@@ -8,3 +9,19 @@ st.header('Bike')
 
 # Sidebar
 st.sidebar.header('Sidebar')
+
+# Menginisiasi data
+categories = ['Casual', 'Registered']
+avg_counts = [avg_casual, avg_registered]
+
+# Pembuatan plot
+fig, ax = plt.subplots(figsize=(8, 5))
+ax.bar(categories, avg_counts, color=['blue', 'green'])
+ax.set_xlabel('Kategori Pengguna Sepeda')
+ax.set_ylabel('Rata-rata Jumlah Pengguna Sepeda')
+ax.set_title('Rata-rata Jumlah Pengguna Sepeda Acak dan Terdaftar')
+ax.grid(axis='y')
+
+# Menampilkan plot di Streamlit
+st.pyplot(fig)
+
