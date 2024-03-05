44b5daa8461a2f96ed9090dc57e47f9a2d5de25c
Sabaria21 <114469120+Sabaria21@users.noreply.github.com>
2024-03-06 00:40:36 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -9,16 +9,4 @@ st.write('Ini adalah dashboard sederhana menggunakan Streamlit.')
 
 # Sidebar
 st.sidebar.header('Sidebar')
-st.sidebar.write('Ini adalah sidebar untuk memilih opsi.')
-
-# Tombol
-if st.button('Klik di sini'):
-    st.write('Anda telah mengklik tombol.')
-
-# Pilihan dari pengguna
-option = st.selectbox('Pilih opsi:', ['A', 'B', 'C'])
-st.write('Anda memilih:', option)
-
-# Masukan dari pengguna
-user_input = st.text_input('Masukkan teks:', 'Tulis sesuatu...')
-st.write('Anda memasukkan:', user_input)
+st.sidebar.write('Ini adalah sidebar')
