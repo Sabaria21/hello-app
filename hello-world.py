6c90e596a5f99c4cb38b86e3da9ba3c23003003e
Sabaria21 <114469120+Sabaria21@users.noreply.github.com>
2024-03-06 00:54:16 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -6,3 +6,8 @@ st.title('Dataset Bike')
 # Teks
 st.header('Pertanyaan 1')
 st.write('Bagaimana perbandingan rata-rata jumlah pengguna sepeda acak dan pengguna sepeda terdaftar.')
+
+
+# Teks 2
+st.header('Pertanyaan 2')
+st.write('Bagaimana pola perubahan penggunaan sepeda selama tahun ('yr') berdasarkan tiap bulannya.')
