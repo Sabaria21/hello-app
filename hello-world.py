e0bb9c0252cafc6b2722899cbfdd7faf01274e1f
Sabaria21 <114469120+Sabaria21@users.noreply.github.com>
2024-03-06 01:02:37 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,39 +1,12 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 
-# Mengimpor file CSV
-@st.cache
-def load_data():
-    df = pd.read_csv("day.csv")
-    return df
+# Judul dashboard
+st.title('Dataset Bike')
 
-def main():
-    # Menampilkan judul
-    st.title("Dashboard Penggunaan Sepeda")
+# Teks
+st.header('Pertanyaan 1')
+st.write('Bagaimana perbandingan rata-rata jumlah pengguna sepeda acak dan pengguna sepeda terdaftar.')
 
-    # Memuat data
-    df = load_data()
-
-    # Menampilkan data jika diinginkan
-    if st.checkbox("Tampilkan Data"):
-        st.write(df)
-
-    # Menghitung rata-rata jumlah pengguna sepeda acak dan terdaftar
-    avg_casual = df['casual'].mean()
-    avg_registered = df['registered'].mean()
-
-    # Pembuatan plot
-    categories = ['Casual', 'Registered']
-    avg_counts = [avg_casual, avg_registered]
-
-    # Plotting menggunakan Matplotlib
-    fig, ax = plt.subplots(figsize=(8, 5))
-    ax.bar(categories, avg_counts, color=['blue', 'green'])
-    ax.set_xlabel('Kategori Pengguna Sepeda')
-    ax.set_ylabel('Rata-rata Jumlah Pengguna Sepeda')
-    ax.set_title('Rata-rata Jumlah Pengguna Sepeda Acak dan Terdaftar')
-    ax.grid(axis='y')
-
-    # Menampilkan plot di Streamlit
-    st.pyplot(fig)
+# Teks
+st.header('Pertanyaan 2')
+st.write('Bagaimana pola perubahan penggunaan sepeda selama tahun berdasarkan tiap bulannya.')
