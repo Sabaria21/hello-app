bd2a297cd39cf08fd211e7a437eac32e58d009bc
Sabaria21 <114469120+Sabaria21@users.noreply.github.com>
2024-03-06 00:53:19 +0800

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,24 +1,8 @@
 import streamlit as st
 
 # Judul dashboard
-st.title('Dashboard Sederhana')
+st.title('Dataset Bike')
 
 # Teks
-st.header('Header Dashboard')
-st.write('Ini adalah dashboard sederhana menggunakan Streamlit.')
-
-# Sidebar
-st.sidebar.header('Sidebar')
-st.sidebar.write('Ini adalah sidebar untuk memilih opsi.')
-
-# Tombol
-if st.button('Klik di sini'):
-    st.write('Anda telah mengklik tombol.')
-
-# Pilihan dari pengguna
-option = st.selectbox('Pilih opsi:', ['A', 'B', 'C'])
-st.write('Anda memilih:', option)
-
-# Masukan dari pengguna
-user_input = st.text_input('Masukkan teks:', 'Tulis sesuatu...')
-st.write('Anda memasukkan:', user_input)
+st.header('Pertanyaan 1')
+st.write('Bagaimana perbandingan rata-rata jumlah pengguna sepeda acak dan pengguna sepeda terdaftar.')
